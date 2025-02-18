d5a8f785c25aa635c955c7aeccf390031ff41c53
MOHD ABUZAR ANSARI <164314033+mohdabuzaransari9@users.noreply.github.com>
2025-02-18 15:49:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
+#importing all libraries
 import streamlit as st
 
+
+#header defining
 st.header('Ai Literary Chatbot')
+
+#expander defining
 with st.expander('Instruction how to use :smile:'):
     st.write('please upload your book through sidebar or use example books')
+
+#placing divider
 st.divider()
+
+#sidebar
+# sidebar uploader
 upload=st.sidebar.file_uploader('upload file',type=['pdf','txt'])
-prompt=st.chat_input("Ask anything")
+
+
+#sidebar book example selectbox
 book_list=['The Adventures of Sherlock Holmes']
 book_selection=st.sidebar.selectbox('select example book to see demo',book_list,index=0)
+
+#sidebar model selectbox
+
 selectbox_options=['RNN','transformer']
 
 selectbox_var=st.sidebar.selectbox('model selection ',selectbox_options,index=1)
@@ -15,7 +30,9 @@ if selectbox_var:
 
     st.sidebar.success('90 percent accuracy')
 
-
+#main content
+#placing prompt chat input
+prompt=st.chat_input('Ask anything')
 if prompt:
     st.write('user prompt:')
     st.write(f'{prompt}')
