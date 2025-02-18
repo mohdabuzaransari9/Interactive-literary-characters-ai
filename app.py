bad9a4bd01285cfb123c5f4e3792a4b37e74353c
MOHD ABUZAR ANSARI <164314033+mohdabuzaransari9@users.noreply.github.com>
2025-02-18 06:09:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ with st.expander('Instruction how to use :smile:'):
     st.write('please upload your book through sidebar or use example books')
 st.divider()
 upload=st.sidebar.file_uploader('upload file',type=['pdf','txt'])
-chat=st.chat_input("Ask anything")
+prompt=st.chat_input("Ask anything")
 book_list=['The Adventures of Sherlock Holmes']
 book_selection=st.sidebar.selectbox('select example book to see demo',book_list,index=0)
 selectbox_options=['RNN','tansformer']
@@ -16,8 +16,8 @@ if selectbox_var:
     st.sidebar.success('90 percent accuracy')
 
 
-if chat:
-    st.write(st.text_area())
+if prompt:
+    st.write(f'user sent the prompt \n{prompt}')
 
 
 
